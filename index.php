52fb2021d9ddc83e14f93226690979e9dec0eef0
MacBook Pro <macbookpro@MacBook-Pro.local>
2020-06-29 20:35:20 +0600

    Inital setup

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Development Area</title>
+	<!-- ALL CSS FILES  -->
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="assets/css/responsive.css">
+</head>
+<body>
+	
+	
+
+	<div class="wrap shadow">
+		<div class="card">
+			<div class="card-body">
+				<h2>Student Registration</h2>
+				<form action="" method="POST">
+					<div class="form-group">
+						<label for="">Name</label>
+						<input name="name" class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Email</label>
+						<input name="email" class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Mobile</label>
+						<input name="mobile" class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<label for="">Age</label>
+						<input name="age" class="form-control" type="text">
+					</div>
+					<div class="form-group">
+						<input class="btn btn-primary" type="submit" value="Register">
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+	
+
+
+
+
+
+
+
+	<!-- JS FILES  -->
+	<script src="assets/js/jquery-3.4.1.min.js"></script>
+	<script src="assets/js/popper.min.js"></script>
+	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="assets/js/custom.js"></script>
+</body>
+</html>
