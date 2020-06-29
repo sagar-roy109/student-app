a8618be19dcd256d0aad584f7d03d3bbf285bfdd
MacBook Pro <macbookpro@MacBook-Pro.local>
2020-06-29 21:01:12 +0600

    Primary Validation with PHP

--- index.php
+++ index.php
@@ -10,6 +10,28 @@
 </head>
 <body>
 	
+	<?php 
+		if(isset($_POST['submit'])){
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$mobile = $_POST['mobile'];
+			$age = $_POST['age'];
+
+			if(empty($name)){
+				$name_error = '<p class = "alert alert-danger"> Name cant be empty</p>';
+			}elseif(empty($email)){
+				$email_error = '<p class = "alert alert-danger"> Email cant be empty</p>';
+			}elseif(empty($mobile)){
+				$mobile_error = '<p class = "alert alert-danger"> Mobile cant be empty</p>';
+			}elseif(empty($age)){
+				$age_error = '<p class = "alert alert-danger"> age cant be empty</p>';
+			}else{
+				$success = '<p class = "alert alert-success"> Registration Successfull</p>';
+			}
+
+		}
+	
+	?>
 	
 
 	<div class="wrap shadow">
@@ -19,23 +41,51 @@
 				<form action="" method="POST">
 					<div class="form-group">
 						<label for="">Name</label>
-						<input name="name" class="form-control" type="text">
+						<input name="name" class="form-control" type="text"><br>
+						<?php 
+							if(isset($name_error)){
+								echo $name_error;
+							}
+						?>
 					</div>
 					<div class="form-group">
 						<label for="">Email</label>
-						<input name="email" class="form-control" type="text">
+						<input name="email" class="form-control" type="text"><br>
+						<?php 
+							if(isset($name_error)){
+								echo $name_error;
+							}
+						?>
 					</div>
 					<div class="form-group">
 						<label for="">Mobile</label>
-						<input name="mobile" class="form-control" type="text">
+						<input name="mobile" class="form-control" type="text"><br>
+						<?php 
+							if(isset($name_error)){
+								echo $name_error;
+							}
+						?>
 					</div>
 					<div class="form-group">
 						<label for="">Age</label>
-						<input name="age" class="form-control" type="text">
+						<input name="age" class="form-control" type="text"><br>
+						<?php 
+							if(isset($name_error)){
+								echo $name_error;
+							}
+						?>
 					</div>
 					<div class="form-group">
-						<input class="btn btn-primary" type="submit" value="Register">
+						<input class="btn btn-primary" name="submit" type="submit" value="Register">
 					</div>
+					<br>	
+
+					<?php 
+						if(isset($success)){
+							echo $success;
+						}
+					?>		
+
 				</form>
 			</div>
 		</div>
